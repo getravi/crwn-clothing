c76e1f01270512cdff49f16bd4ab0288c449b355
getravi <getravi@gmail.com>
2020-09-08 22:01:21 -0700

    added routing to the ecommerce app

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import MenuItem from "../menu-item/menu-item.component";
 
-import '../directory/directory.styles.scss';
+import "../directory/directory.styles.scss";
 
 class Directory extends React.Component {
   constructor() {
@@ -48,8 +48,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id , size}) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}></MenuItem>
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps}></MenuItem>
         ))}
       </div>
     );
